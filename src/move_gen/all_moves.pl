d41805ae446668e3bd300741c9955097f8d53f0c
Matthias Seghers <matthias.h.seghers@gmail.com>
2024-05-02 09:07:15 +0200

    directional moves

--- src/move_gen/all_moves.pl
+++ src/move_gen/all_moves.pl
@@ -14,7 +14,7 @@ all_moves(Board, MetData, Color, Moves):-
     findall(
         PieceMoves,
         (
-            get_piece_at(Coord, Board, p(Color, Type)),
+            get_piece_at(Coord, Board, p(Color, Type)), % this doesn't select p(empty) pieces because we ask a p/2.
             all_moves_of_piece(Board, MetData, Coord, p(Color, Type), PieceMoves)
         ),
         PieceMovesList
@@ -24,19 +24,19 @@ all_moves(Board, MetData, Color, Moves):-
 % all_moves_of_piece(+Board, +MetData, +Coord, +Piece, -Moves)
 % get all the moves for the given piece at the given coordinates.
 all_moves_of_piece(Board, _, Coord, p(Color, rook), Hz-R):-
-    horizontal_moves(Board, Coord, p(Color, rook), Hz-Vt),
-    vertical_moves(Board, Coord, p(Color, rook), Vt-R), !.
+    horizontal_moves(Board, Coord, Color, Hz-Vt),
+    vertical_moves(Board, Coord, Color, Vt-R), !.
 all_moves_of_piece(Board, _, Coord, p(Color, bishop), Bishop_moves):-
-    diagonal_moves(Board, Coord, p(Color, bishop), Bishop_moves), !.
+    diagonal_moves(Board, Coord, Color, Bishop_moves), !.
 all_moves_of_piece(Board, _, Coord, p(Color, knight), Knight_moves):-
-    knight_moves(Board, Coord, p(Color, knight), Knight_moves), !.
+    knight_moves(Board, Coord, Color, Knight_moves), !.
 all_moves_of_piece(Board, _, Coord, p(Color, queen), Hz-R):-
-    horizontal_moves(Board, Coord, p(Color, queen), Hz-Vt),
-    vertical_moves(Board, Coord, p(Color, queen), Vt-Diag),
-    diagonal_moves(Board, Coord, p(Color, queen), Diag-R), !.
+    horizontal_moves(Board, Coord, Color, Hz-Vt),
+    vertical_moves(Board, Coord, Color, Vt-Diag),
+    diagonal_moves(Board, Coord, Color, Diag-R), !.
 all_moves_of_piece(Board, meta(Wm, _, _), Coord, p(white, king), King_moves):-
-    king_moves(Board, Wm, Coord, p(Color, king), King_moves), !.
+    king_moves(Board, Wm, Coord, Color, King_moves), !.
 all_moves_of_piece(Board, meta(_, Bm, _), Coord, p(black, king), King_moves):-
-    king_moves(Board, Bm, Coord, p(Color, king), King_moves), !.
+    king_moves(Board, Bm, Coord, Color, King_moves), !.
 all_moves_of_piece(Board, meta(_, _, LastMove), Coord, p(Color, pawn), Pawn_moves):-
-    pawn_moves(Board, LastMove, Coord, p(Color, pawn), Pawn_moves), !.
+    pawn_moves(Board, LastMove, Coord, Color, Pawn_moves), !.
--- src/move_gen/directional.pl
+++ src/move_gen/directional.pl
@@ -1,5 +1,55 @@
 :- module(directional, [horizontal_moves/4, vertical_moves/4, diagonal_moves/4]).
 
-horizontal_moves(_, _, _, _).
-vertical_moves(_, _, _, _).
-diagonal_moves(_, _, _, _).
+:- use_module('../utils/board_utils').
+
+% increment(?A, ?B)
+% is B = A + 1?
+increment(A, B) :- B is A + 1.
+% inc_row(?R, ?NR)
+% increment the row
+inc_row(R/_, NR/_):- increment(R, NR).
+%inc_col(?C, ?NC)
+% increment the column
+inc_col(_/C, _/NC):- increment(C, NC).
+
+% horizontal_moves(+Board, +Coord, +Color, -HMoves)
+% get all the horizontal moves for the piece at the given coordinates.
+horizontal_moves(Board, Coord, Color, HMoves):-
+    % traverse the columns
+    both_way_moves(inc_col, Board, Coord, Color, HMoves).
+
+% vertical_moves(+Board, +Coord, +Color, -VMoves)
+% get all the vertical moves for the piece at the given coordinates.
+vertical_moves(Board, Coord, Color, VMoves):-
+    % traverse the rows
+    both_way_moves(inc_row, Board, Coord, Color, VMoves).
+
+% diagonal_moves(+Board, +Coord, +Color, -DMoves)
+% get all the diagonal moves for the piece at the given coordinates.
+diagonal_moves(Board, Coord, Color, LU-X):-
+    both_way_moves([Old, New]>>(inc_row(Old, Temp), inc_col(Temp, New)), Board, Coord, Color, LU-RU),
+    both_way_moves([Old, New]>>(inc_row(Old, Temp), inc_col(New, Temp)), Board, Coord, Color, RU-X).
+
+% both_way_moves(+Next, +Board, +Coord, +Color, -Moves)
+% get all the moves in both directions of OneWayNext for the given coordinates.
+both_way_moves(OneWayNext, Board, Coord, Color, OneWay-X):-
+    % goes in one way
+    propagate(OneWayNext, Board, Color, Coord, Coord, OneWay-OtherWay),
+    % goes in the other way (the opposite of the first way)
+    propagate([Old, New]>>(call(OneWayNext, New, Old)), Board, Color, Coord, Coord, OtherWay-X).
+
+
+% propagate(+Next, +Board, +Color, +Origin, +CurCoor, -Moves)
+% propagates the move in the direction given by Next/2, until it can't no more.
+% returns all moves along this axis.
+
+% stop if move is illegal
+propagate(_, _, _, _, CurCoor, X-X):- is_illegal_coord(CurCoor), !.
+% stop if we hit a piece of the same color
+propagate(_, Board, Color, _, CurCoor, X-X):- get_piece_at(CurCoor, Board, p(Color, _)), !.
+% capture piece of the opposite color and stop (this doesn't capture empty pieces, because we ask for p/2 not a p/1)
+propagate(Next, Board, Color, Origin, CurCoor, [move(Origin, CurCoor) | X]-X):- get_piece_at(CurCoor, Board, p(_, _)), !.
+% the tile is empty, continue propagating
+propagate(Next, Board, Color, Origin, CurCoor, [move(Origin, CurCoor)| Next]-X):-
+    call(Next, CurCoor, NextCoord),
+    propagate(Next, Board, Color, Origin, NextCoord, Next-X), !.
